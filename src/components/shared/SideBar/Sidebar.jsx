a21c5a8522c5b6bc1c10f7c5703bef46c6c8eb89
fayshal <imfayshalrana@gmail.com>
2024-02-18 14:25:47 +0600

    change sideber items icon

--- src/components/shared/SideBar/Sidebar.jsx
+++ src/components/shared/SideBar/Sidebar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import logo from '../../../assets/images/logo.svg'
 import { HiOutlineViewGrid } from "react-icons/hi";
-import { RiTableLine } from "react-icons/ri";
-import { RiPagesLine } from "react-icons/ri";
+import { RiTableLine, RiPagesLine } from "react-icons/ri";
+import { IoMailOutline } from "react-icons/io5";
+import { CiInboxIn } from "react-icons/ci";
+import { GrPieChart } from "react-icons/gr";
+import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
+import { TbCircuitCellPlus } from "react-icons/tb";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { LuLogIn } from "react-icons/lu";
 import { FiHome, FiUser } from "react-icons/fi";
 import { FaTasks } from "react-icons/fa";
 import { Link } from 'react-router-dom';
@@ -32,9 +37,9 @@ const Sidebar = () => {
                     <div className="">
                         <h3 className='mb-4 ml-4 text-sm font-medium text-secondary uppercase'>Menu</h3>
                         <ul className='flex flex-col gap-1.5'>
-                            <li><Link to="/" className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover' ><HiOutlineViewGrid />Dashboard</Link></li>
-                            <li><Link to="/profile" className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover'><FiUser />Profile</Link></li>
-                            <li><Link onClick={() => handleMenuClick('menu1')} className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover ${activeMenu === 'menu1' ? "bg-menuHover" : ""}`} href=""><FaTasks />Forms <MdKeyboardArrowDown className={`ml-auto text-xl ${activeMenu === 'menu1' ? "rotate-180" : ""}`} /></Link></li>
+                            <li><Link to="/" className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover' ><HiOutlineViewGrid className='text-xl' />Dashboard</Link></li>
+                            <li><Link to="/profile" className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover'><FiUser className='text-xl' />Profile</Link></li>
+                            <li><Link onClick={() => handleMenuClick('menu1')} className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover ${activeMenu === 'menu1' ? "bg-menuHover" : ""}`} href=""><FaTasks className='text-xl' />Forms <MdKeyboardArrowDown className={`ml-auto text-xl ${activeMenu === 'menu1' ? "rotate-180" : ""}`} /></Link></li>
 
 
                             {/* Sub-menus  */}
@@ -46,7 +51,7 @@ const Sidebar = () => {
                                 </ul>
                             )}
 
-                            <li><Link onClick={() => handleMenuClick('menu2')} className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover ${activeMenu === 'menu2' ? "bg-menuHover" : ""}`} href=""><FaTasks />Table <MdKeyboardArrowDown className={`ml-auto text-xl ${activeMenu === 'menu2' ? "rotate-180" : ""}`} /></Link></li>
+                            <li><Link onClick={() => handleMenuClick('menu2')} className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover ${activeMenu === 'menu2' ? "bg-menuHover" : ""}`} href=""><RiTableLine className='text-xl' />Table <MdKeyboardArrowDown className={`ml-auto text-xl ${activeMenu === 'menu2' ? "rotate-180" : ""}`} /></Link></li>
 
                             {/* Sub-menus  */}
                             {activeMenu === 'menu2' && (
@@ -57,7 +62,7 @@ const Sidebar = () => {
                                 </ul>
                             )}
 
-                            <li><Link onClick={() => handleMenuClick('menu3')} className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover ${activeMenu === 'menu3' ? "bg-menuHover" : ""}`} href=""><FaTasks />pages <MdKeyboardArrowDown className={`ml-auto text-xl ${activeMenu === 'menu3' ? "rotate-180" : ""}`} /></Link></li>
+                            <li><Link onClick={() => handleMenuClick('menu3')} className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover ${activeMenu === 'menu3' ? "bg-menuHover" : ""}`} href=""><RiPagesLine className='text-xl' />pages <MdKeyboardArrowDown className={`ml-auto text-xl ${activeMenu === 'menu3' ? "rotate-180" : ""}`} /></Link></li>
 
                             {/* Sub-menus  */}
                             {activeMenu === 'menu3' && (
@@ -75,17 +80,17 @@ const Sidebar = () => {
                     <div className="">
                         <h3 className='mb-4 ml-4 text-sm font-medium text-secondary uppercase'>SUPPORT</h3>
                         <ul className='flex flex-col gap-1.5'>
-                            <li><Link className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover' href=""><HiOutlineViewGrid />Messages</Link></li>
-                            <li><Link className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover' href=""><FiUser />Inbox</Link></li>
-                            <li><Link className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover' href=""><FiUser />invoice</Link></li>
+                            <li><Link className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover' href=""><IoMailOutline className='text-xl' />Messages</Link></li>
+                            <li><Link className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover' href=""><CiInboxIn className='text-xl' />Inbox</Link></li>
+                            <li><Link className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover' href=""><LiaFileInvoiceDollarSolid className='text-xl' />invoice</Link></li>
                         </ul>
                     </div>
                     {/* others Group  */}
                     <div className="">
                         <h3 className='mb-4 ml-4 text-sm font-medium text-secondary uppercase'>Others</h3>
                         <ul className='flex flex-col gap-1.5'>
-                            <li><Link className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover' href=""><HiOutlineViewGrid />Charts</Link></li>
-                            <li><Link onClick={() => handleMenuClick('menu4')} className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover ${activeMenu === 'menu4' ? "bg-menuHover" : ""}`} href=""><FaTasks />UI Elements <MdKeyboardArrowDown className={`ml-auto text-xl ${activeMenu === 'menu4' ? "rotate-180" : ""}`} /></Link></li>
+                            <li><Link className='className="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover' href=""><GrPieChart className='text-xl' />Charts</Link></li>
+                            <li><Link onClick={() => handleMenuClick('menu4')} className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover ${activeMenu === 'menu4' ? "bg-menuHover" : ""}`} href=""><TbCircuitCellPlus className='text-xl' />UI Elements <MdKeyboardArrowDown className={`ml-auto text-xl ${activeMenu === 'menu4' ? "rotate-180" : ""}`} /></Link></li>
 
 
                             {/* Sub-menus  */}
@@ -110,7 +115,7 @@ const Sidebar = () => {
 
                                 </ul>
                             )}
-                            <li><Link onClick={() => handleMenuClick('menu5')} className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover ${activeMenu === 'menu5' ? "bg-menuHover" : ""}`} href=""><FaTasks />Authentication <MdKeyboardArrowDown className={`ml-auto text-xl ${activeMenu === 'menu5' ? "rotate-180" : ""}`} /></Link></li>
+                            <li><Link onClick={() => handleMenuClick('menu5')} className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-white duration-300 ease-in-out hover:bg-menuHover ${activeMenu === 'menu5' ? "bg-menuHover" : ""}`} href=""><LuLogIn className='text-xl' />Authentication <MdKeyboardArrowDown className={`ml-auto text-xl ${activeMenu === 'menu5' ? "rotate-180" : ""}`} /></Link></li>
 
 
                             {/* Sub-menus  */}

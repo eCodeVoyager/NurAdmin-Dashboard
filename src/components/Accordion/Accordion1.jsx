9d572da4f6251c12c41728c21921f76e73081874
fayshal <imfayshalrana@gmail.com>
2024-02-21 20:32:09 +0600

    accordion style 1 added on ui component

---
+++ src/components/Accordion/Accordion1.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { IoIosArrowDown } from "react-icons/io";
+
+const Accordion1 = ({ open, setOpen }) => {
+    const toggleAccordion = () => {
+        setOpen(prevOpen => !prevOpen);
+    };
+
+    return (
+        <div className="rounded-md border border-stroke p-4 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] sm:p-6">
+            <button onClick={toggleAccordion} className="flex w-full items-center gap-1.5 sm:gap-3 xl:gap-6">
+                <div className="flex h-10 w-10 items-center justify-center rounded-md bg-[#F3F5FC]">
+                    <IoIosArrowDown className={`text-primary text-2xl duration-300 ${open ? "rotate-180" : ""}`} />
+                </div>
+                <div>
+                    <h4 className="text-left text-lg font-medium text-black">
+                        How long we deliver your first blog post?
+                    </h4>
+                </div>
+            </button>
+            {open && <div className="md:ml-16 mt-5 duration-200 ease-in-out">
+                <p className="font-medium text-secondary">
+                    It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research &amp;
+                    creation of your monthly content marketing strategy that we do before writing your first blog post,
+                    Ipsum available.
+                </p>
+            </div>}
+        </div>
+    );
+}
+
+export default Accordion1;
--- src/pages/UiElements/Accordion.jsx
+++ src/pages/UiElements/Accordion.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import BredCrumb from '../../components/shared/Buredcrumb/BredCrumb';
+import Accordion1 from '../../components/Accordion/Accordion1';
 
 const Accordion = () => {
+  const [openAccordionIndex, setOpenAccordionIndex] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setOpenAccordionIndex(prevIndex => prevIndex === index ? null : index);
+  };
+
   return (
-    <div>
-      Accordion
+    <div className='container_fluid'>
+      <div className="mx-auto max-w-242.5">
+        <BredCrumb pageName={"Accordion"} />
+        <div className="flex flex-col gap-7.5">
+          <div className="rounded-sm border border-stroke bg-white shadow-default">
+            <div className="border-b border-stroke px-4 py-4 sm:px-6 xl:px-9">
+              <h3 className="font-medium text-black">Accordions Style 1</h3>
+            </div>
+            <div className="p-4 sm:p-6 xl:p-9">
+              <div className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2 xl:gap-7.5">
+                <div className="flex flex-col gap-6">
+                  <Accordion1 open={openAccordionIndex === 0} setOpen={() => toggleAccordion(0)} />
+                  <Accordion1 open={openAccordionIndex === 1} setOpen={() => toggleAccordion(1)} />
+                </div>
+                <div className="flex flex-col gap-6">
+                  <Accordion1 open={openAccordionIndex === 2} setOpen={() => toggleAccordion(2)} />
+                  <Accordion1 open={openAccordionIndex === 3} setOpen={() => toggleAccordion(3)} />
+                  <Accordion1 open={openAccordionIndex === 4} setOpen={() => toggleAccordion(4)} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Accordion
+export default Accordion;

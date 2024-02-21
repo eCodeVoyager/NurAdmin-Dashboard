c8d33062d2bf0b3b0aebc519fd3ab7965339f0fb
fayshal <imfayshalrana@gmail.com>
2024-02-22 02:49:27 +0600

    ui element alerts added

--- src/pages/UiElements/Alerts.jsx
+++ src/pages/UiElements/Alerts.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import BredCrumb from '../../components/shared/Buredcrumb/BredCrumb'
+import AttentionNeeded from '../../components/Alerts/AttentionNeeded'
+import MessageSentSuccessfully from '../../components/Alerts/MessageSentSuccessfully'
+import ErrorMessage from '../../components/Alerts/ErrorMessage'
 
 const Alerts = () => {
   return (
-    <div>
-      Alerts
+    <div className='container_fluid'>
+      <div className="mx-auto max-w-242.5">
+        <BredCrumb pageName={"Alerts"} />
+        <div className="rounded-sm border border-stroke bg-white p-4 shadow-default md:p-6 xl:p-9">
+          <div className="flex flex-col gap-7">
+            {/* Alerts Item  */}
+            <AttentionNeeded />
+            {/* Alerts Item  */}
+            <MessageSentSuccessfully />
+            {/* Alerts Item  */}
+           <ErrorMessage/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

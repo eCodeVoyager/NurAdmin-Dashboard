fb002704230dde072582a7bfc587c655b38618d8
fayshal <imfayshalrana@gmail.com>
2024-02-23 02:56:13 +0600

    button group with icon added on ui element page

--- src/components/ButtonGroup/NormalButtonGroup.jsx
+++ src/components/ButtonGroup/NormalButtonGroup.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import cn from '../../utils/cn'
 
-const NormalButtonGroup = ({buttonOneClass, buttonTwoClass, buttonThreeClass}) => {
+const NormalButtonGroup = ({ buttonOneClass, buttonTwoClass, buttonThreeClass, buttonOneText, buttonTwoText, buttonThreeText, firstIcon, secondIcon, thirdIcon }) => {
     return (
         <div class="flex items-center">
-            <a href="#" class={cn("inline-flex border border-primary bg-primary px-2 py-1 font-medium text-white hover:border-primary hover:bg-primary hover:text-white sm:px-6 sm:py-3", buttonOneClass)}>About</a>
-            <a href="#" class={cn("inline-flex border-y border-stroke px-2 py-1 font-medium text-black hover:border-primary hover:bg-primary hover:text-white  sm:px-6 sm:py-3", buttonTwoClass)}>Profile</a>
-            <a href="#" class={cn("inline-flex border border-stroke px-2 py-1 font-medium text-black hover:border-primary hover:bg-primary hover:text-white dark:border-strokedark sm:px-6 sm:py-3", buttonThreeClass)}>Services</a>
+            <a href="#" class={cn("inline-flex border border-primary bg-primary px-2 py-1 font-medium text-white hover:border-primary hover:bg-primary hover:text-white sm:px-6 sm:py-3 items-center gap-2", buttonOneClass)}>{firstIcon}{buttonOneText}</a>
+            <a href="#" class={cn("inline-flex border-y border-stroke px-2 py-1 font-medium text-black hover:border-primary hover:bg-primary hover:text-white  sm:px-6 sm:py-3 items-center gap-2", buttonTwoClass)}>{secondIcon}{buttonTwoText}</a>
+            <a href="#" class={cn("inline-flex border border-stroke px-2 py-1 font-medium text-black hover:border-primary hover:bg-primary hover:text-white dark:border-strokedark sm:px-6 sm:py-3 items-center gap-2", buttonThreeClass)}>{thirdIcon}{buttonThreeText}</a>
         </div>
     )
 }
--- src/pages/UiElements/ButtonGroup.jsx
+++ src/pages/UiElements/ButtonGroup.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import NormalButtonGroup from '../../components/ButtonGroup/NormalButtonGroup'
 import BredCrumb from '../../components/shared/Buredcrumb/BredCrumb'
-
+import { RiUser6Line } from "react-icons/ri";
+import { LiaMoneyBillWaveAltSolid } from "react-icons/lia";
+import { TfiRulerPencil } from "react-icons/tfi";
 const ButtonGroup = () => {
   return (
     <div className='container_fluid'>
@@ -18,9 +20,28 @@ const ButtonGroup = () => {
           </div>
           <div class="p-4 sm:p-5 xl:p-7.5">
             <div class="flex flex-col gap-6">
-              <NormalButtonGroup />
-              <NormalButtonGroup buttonOneClass={"rounded-s-md"} buttonThreeClass={"rounded-e-md"} />
-              <NormalButtonGroup buttonThreeClass={"rounded-e-full"} buttonOneClass={"rounded-s-full"} />
+              <NormalButtonGroup buttonOneText={"About"} buttonTwoText={"Profile"} buttonThreeText={"Services"} />
+              <NormalButtonGroup buttonOneText={"About"} buttonTwoText={"Profile"} buttonThreeText={"Services"} buttonOneClass={"rounded-s-md"} buttonThreeClass={"rounded-e-md"} />
+              <NormalButtonGroup buttonOneText={"About"} buttonTwoText={"Profile"} buttonThreeText={"Services"} buttonThreeClass={"rounded-e-full"} buttonOneClass={"rounded-s-full"} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      {/* Buttons Group with icon*/}
+      <div className="flex flex-col gap-7">
+        <div className="rounded-sm border border-stroke bg-white shadow-default">
+          <div class="border-b border-stroke px-4 py-4 sm:px-6 xl:px-7.5">
+            <h3 class="font-medium text-black">
+              Buttons Group
+            </h3>
+          </div>
+          <div class="p-4 sm:p-5 xl:p-7.5">
+            <div class="flex flex-col gap-6">
+              <NormalButtonGroup buttonOneText={"About"} buttonTwoText={"Profile"} buttonThreeText={"Services"} firstIcon={<RiUser6Line className='text-xl' />} secondIcon={<LiaMoneyBillWaveAltSolid className='text-2xl' />} thirdIcon={<TfiRulerPencil className='text-xl' />} />
+              <NormalButtonGroup buttonOneText={"About"} buttonTwoText={"Profile"} buttonThreeText={"Services"} buttonOneClass={"rounded-s-md"} buttonThreeClass={"rounded-e-md"} firstIcon={<RiUser6Line className='text-xl' />} secondIcon={<LiaMoneyBillWaveAltSolid className='text-2xl' />} thirdIcon={<TfiRulerPencil className='text-xl' />}/>
+              <NormalButtonGroup buttonOneText={"About"} buttonTwoText={"Profile"} buttonThreeText={"Services"} buttonThreeClass={"rounded-e-full"} buttonOneClass={"rounded-s-full"} firstIcon={<RiUser6Line className='text-xl' />} secondIcon={<LiaMoneyBillWaveAltSolid className='text-2xl' />} thirdIcon={<TfiRulerPencil className='text-xl' />}/>
             </div>
           </div>
         </div>

fc0ba7ca6f312ba9ab6051872170f55331150b1b
fayshal <imfayshalrana@gmail.com>
2024-02-23 00:54:01 +0600

    Breadcrumb  style one added on ui element

--- src/pages/UiElements/Breadcrumb.jsx
+++ src/pages/UiElements/Breadcrumb.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import BredCrumb from '../../components/shared/Buredcrumb/BredCrumb'
+import Breadcrumb1 from '../../components/Breadcrumb/Style1/Breadcrumb1'
 
 const Breadcrumb = () => {
   return (
-    <div>
-      Breadcrumb
+    <div className='container_fluid'>
+      <div className="mx-auto max-w-242.5">
+        <BredCrumb pageName={"Breadcrumb"} />
+        <div className="flex flex-col gap-7">
+          {/* Style one  */}
+          <div class="rounded-sm border border-stroke bg-white shadow-default">
+                <div class="border-b border-stroke px-4 py-4 sm:px-6 xl:px-7">
+                  <h3 class="font-medium text-black">
+                    Breadcrumb Style 1
+                  </h3>
+                </div>
+
+                <div class="p-4 sm:p-6 xl:p-10">
+                  <Breadcrumb1/>
+                </div>
+              </div>
+        </div>
+      </div>
     </div>
   )
 }

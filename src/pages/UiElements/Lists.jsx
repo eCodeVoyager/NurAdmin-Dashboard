eb4ea0df9bc3afc187a4d05ca8505194f079949b
fayshal <imfayshalrana@gmail.com>
2024-02-23 17:53:13 +0600

    List style 2 added on ui element page

--- src/pages/UiElements/Lists.jsx
+++ src/pages/UiElements/Lists.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BredCrumb from '../../components/shared/Buredcrumb/BredCrumb'
 import ListStyle1 from '../../components/Lists/ListStyle1'
+import ListStyle2 from '../../components/Lists/ListStyle2'
 
 const Lists = () => {
   return (
@@ -8,6 +9,7 @@ const Lists = () => {
       <div className="mx-auto max-w-242.5">
         <BredCrumb pageName={"Lists"} />
         <div className="flex flex-col gap-7">
+          {/* list Style one  */}
           <div className="rounded-sm border border-stroke bg-white shadow-default">
             <div className="border-b border-stroke px-4 py-4 sm:px-6 xl:px-9">
               <h3 class="font-medium text-black">
@@ -16,6 +18,17 @@ const Lists = () => {
             </div>
             <div class="p-4 sm:p-6 xl:p-9">
               <ListStyle1 />
+            </div>
+          </div>
+          {/* list Style Two  */}
+          <div className="rounded-sm border border-stroke bg-white shadow-default">
+            <div className="border-b border-stroke px-4 py-4 sm:px-6 xl:px-9">
+              <h3 class="font-medium text-black">
+                Style 2
+              </h3>
+            </div>
+            <div class="p-4 sm:p-6 xl:p-9">
+              <ListStyle2 />
             </div>
           </div>
         </div>

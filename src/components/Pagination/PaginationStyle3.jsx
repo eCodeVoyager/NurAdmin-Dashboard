156087d234a4c02b9798a7b41aedade266fa690a
fayshal <imfayshalrana@gmail.com>
2024-02-27 19:58:54 +0600

    pagination style added on ui component page

---
+++ src/components/Pagination/PaginationStyle3.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const PaginationStyle3 = () => {
+  return (
+    <nav>
+    <ul class="flex flex-wrap items-center gap-2">
+      <li>
+        <a class="flex items-center justify-center rounded bg-[#EDEFF1] px-3 py-1.5 text-xs font-medium text-black hover:bg-primary hover:text-white " href="#">
+          Previous
+        </a>
+      </li>
+      <li>
+        <a class="flex items-center justify-center rounded px-3 py-1.5 font-medium hover:bg-primary text-secondary hover:text-white" href="#">1</a>
+      </li>
+      <li>
+        <a class="flex items-center justify-center rounded px-3 py-1.5 font-medium hover:bg-primary text-secondary hover:text-white" href="#">2</a>
+      </li>
+      <li>
+        <a class="flex items-center justify-center rounded px-3 py-1.5 font-medium hover:bg-primary text-secondary hover:text-white" href="#">3</a>
+      </li>
+      <li>
+        <a class="flex items-center justify-center rounded px-3 py-1.5 font-medium hover:bg-primary text-secondary hover:text-white" href="#">4</a>
+      </li>
+      <li>
+        <a class="flex items-center justify-center rounded px-3 py-1.5 font-medium hover:bg-primary text-secondary hover:text-white" href="#">5</a>
+      </li>
+      <li>
+        <a class="flex items-center justify-center rounded bg-[#EDEFF1] px-3 py-1.5 text-xs font-medium text-black hover:bg-primary hover:text-white" href="#">
+          Next
+        </a>
+      </li>
+    </ul>
+  </nav>
+  )
+}
+
+export default PaginationStyle3
--- src/pages/UiElements/Pagination.jsx
+++ src/pages/UiElements/Pagination.jsx
@@ -1,13 +1,58 @@
 import React from 'react'
 import BredCrumb from '../../components/shared/Buredcrumb/BredCrumb'
+import PaginationStyle1 from '../../components/Pagination/PaginationStyle1'
+import PaginationStyle2 from '../../components/Pagination/PaginationStyle2'
+import PaginationStyle3 from '../../components/Pagination/PaginationStyle3'
 
 const Pagination = () => {
   return (
     <div className='container_fluid'>
-    <div className="mx-auto max-w-242.5">
-      <BredCrumb pageName={"Pagination"} />
+      <div className="mx-auto max-w-242.5">
+        <BredCrumb pageName={"Pagination"} />
+
+
+        <div className="flex flex-col gap-7">
+          {/* pagination style one  */}
+          <div class="rounded-sm border border-stroke bg-white shadow-default">
+            <div class="border-b border-stroke px-4 py-4 sm:px-6 xl:px-7.5">
+              <h3 class="font-medium text-black">
+                Pagination Style 1
+              </h3>
+            </div>
+
+            <div class="p-4 sm:p-6 xl:p-7">
+              <PaginationStyle1 />
+            </div>
+          </div>
+
+          {/* pagination style two  */}
+          <div class="rounded-sm border border-stroke bg-white shadow-default">
+            <div class="border-b border-stroke px-4 py-4 sm:px-6 xl:px-7.5">
+              <h3 class="font-medium text-black">
+                Pagination Style 2
+              </h3>
+            </div>
+
+            <div class="p-4 sm:p-6 xl:p-7">
+              <PaginationStyle2 />
+            </div>
+          </div>
+
+          {/* pagination style three  */}
+          <div class="rounded-sm border border-stroke bg-white shadow-default">
+            <div class="border-b border-stroke px-4 py-4 sm:px-6 xl:px-7.5">
+              <h3 class="font-medium text-black ">
+                Pagination Style 3
+              </h3>
+            </div>
+
+            <div class="p-4 sm:p-6 xl:p-7">
+              <PaginationStyle3 />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
   )
 }
 
